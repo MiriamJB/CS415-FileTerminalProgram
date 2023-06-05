21e105f70c18649ba242ce05c7102b9ab6d125f5
MiriamJB <mjb376@ensign.edu>
2023-06-05 16:35:32 -0600

    feat: user manuel

--- main.cpp
+++ main.cpp
@@ -2,23 +2,58 @@
 #include <cstring>
 #include <string>
 #include  <bits/stdc++.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+//displays avalable commands
+void userManuel() {
+    std::cout
+        << "-------------------USER MANUEL-------------------\n"
+        << "      ***Commands ARE case-sensitive***\n"
+        << "? OR help                Displays the user manuel\n"
+        << "create                 Opens the Create File menu\n"
+        << "read                     Opens the Read File menu\n"
+        << "write                   Opens the Write File menu\n"
+        << "delete                 Opens the Delete File menu\n"
+        << "quit              Stops the File Terminal Program\n"
+        << "\n";
+}
+
+void createMenu() {
+
+}
 
 int main() {
+    std::string command;
     std::string input;
+    bool running = true;
     char* prompt = ">";
     
+    //welcome message
     std::cout << "\nWelcome to Miriam's File Terminal Program. You may enter a command." << std::endl;
-    std::cout << prompt;
-    std::getline(std::cin, input);
     
-    if (input == "?") {
-        userManuel();
+    //this is the loop that runs the program
+    while (running) {
+        //the loop starts by printing out the prompt
+        std::cout << prompt;
+
+        //read input from the user
+        std::getline(std::cin, input);
+        command = input; /////////////////////TODO: get first word from input
+    
+        if (input == "") {
+            //if the user simply presses enter, it will re-start the loop
+        } else if (input == "?" || input == "help") {
+            userManuel();
+        } else if (input == "create") {
+            createMenu();
+        } else if (input == "quit") {
+            running = false;
+        } else {
+            std::cout << "Error: invalid command.\n";
+        }
     }
     
     return 0;
-}
-
-void userManuel() {
-    std::cout << "------------------------USER MANUEL------------------------\n"
-        << "\t";
 }
