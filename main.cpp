42085bc3f6b1df350dc363f1cb038649c259d4f8
MiriamJB <mjb376@ensign.edu>
2023-06-07 17:43:47 -0600

    implementation of read, write, and delete file

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 
 namespace fs = std::filesystem;
-char fileDirectory[50];
+std::string fileDirectory[50];
 int fileIndex;
 
 //displays avalable commands
@@ -16,39 +16,111 @@ void userManuel() {
         << "-------------------------------USER MANUEL-------------------------------\n"
         << "                  ***Commands ARE case-sensitive***\n"
         << "? OR help                                        Displays the user manuel\n"
-        << "showfiles                                Lists all files in the directory\n"
+        << "directory                                Lists all files in the directory\n"
         << "create <fileName>                    Creates a file with the desired name\n"
         << "read <fileName>                  Shows the contents of the specified file\n"
         << "write <fileName>                             Writes to the specified file\n"
         << "delete <fileName>                              Deletes the specified file\n"
         << "quit                                      Stops the File Terminal Program\n"
-        << "\n";
+        << "-------------------------------------------------------------------------\n"
+        << std::endl;
+}
+
+//prints all the files that are stored in fileDirectory
+void showFileDirectory() {
+    if (fileIndex == 0) {
+        std::cout << "The file directory is empty." << std::endl;
+    } else {
+        std::cout << "------------------------------FILE DIRECTORY------------------------------" << std::endl;
+        for (int i = 0; i < fileIndex; i++)
+        std::cout << fileDirectory[i] << std::endl;
+        std::cout << "--------------------------------------------------------------------------\n" << std::endl;
+    }
 }
 
 //will create a file in the "files" folder
 void createFile(const std::string& fileName) {
-    std::ofstream file("files/" + fileName);
-    if (file) {
-        std::cout << fileName << " created successfully." << std::endl;
-        file.close();
-        //////////////////////////////// TODO: add created files to directory
+    if (fileIndex > 50) {
+        std::cerr << "ERROR: could not create \"" << fileName << "\". The directory cannot hold more than 50 files.\n" << std::endl; 
     } else {
-        std::cerr << "ERROR: could not create \"" << fileName << "\"." << std::endl;
+        std::ofstream file("files/" + fileName);
+        if (file) {
+            std::cout << fileName << " created successfully.\n" << std::endl;
+            file.close();
+            fileDirectory[fileIndex] = fileName;
+            fileIndex++;
+        } else {
+            std::cerr << "ERROR: failed to create \"" << fileName << "\".\n" << std::endl;
+        }
     }
 }
 
-////////////////////////////////////////////////////// TODO: refine readfile
+//prints the contents of the file
 void readFile(const std::string& fileName) {
     std::ifstream file("files/" + fileName);
     if (file) {
+        std::cout << "\nExisting content of \"" << fileName << "\":" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
         std::string line;
         while (std::getline(file, line)) {
             std::cout << line << std::endl;
         }
+        std::cout << "--------------------------------------------------------------------------\n" << std::endl;
         file.close();
     } else {
-        std::cerr << "Failed to open file: " << fileName << std::endl;
+        std::cerr << "ERROR: failed to open file \"" << fileName << "\".\n" << std::endl;
     }
+}
+
+//shows the current content of the file and lets the user overwrite it
+void writeFile(const std::string& fileName) {
+    std::ifstream file("files/" + fileName);
+    if (file) {
+        std::string content;
+        std::string line;
+        while (std::getline(file, line)) {
+            content += line + "\n";
+        }
+        file.close();
+
+        readFile(fileName);
+        std::cout << "Enter the modified content. (Type \"SAVE\" to save and exit.)" << std::endl;
+
+        content.clear();
+        while (std::getline(std::cin, line)) {
+            if (line == "SAVE") {
+                std::ofstream file("files/" + fileName);
+                file << content;
+                std::cout << "Content saved to file \"" << fileName << "\".\n" << std::endl;
+                file.close();
+                break;
+            }
+            content += line + "\n";
+        }
+    } else {
+        std::cerr << "ERROR: failed to open file \"" << fileName << "\".\n" << std::endl;
+    }
+}
+
+//asks if the user is sure about deletion and then deletes the file accordingly
+void deleteFile(const std::string& fileName) {
+    std::cout << "Are you sure you want to delete \"" << fileName << "\"? (Y/N) ";
+    std::string response;
+    std::getline(std::cin, response);
+
+    if (response == "N" || response == "n") {
+        std::cout << "File \"" << fileName << "\" has NOT been deleted.\n" << std::endl;
+    } else if (response == "Y" || response == "y") {
+        if (fs::remove("files/" + fileName)) {
+            std::cout << "File \"" << fileName << "\" has been deleted.\n" << std::endl;
+        } else {
+        std::cerr << "ERROR: failed to delete file: \"" << fileName << "\".\n" << std::endl;
+        }
+    } else {
+        std::cerr << "ERROR: invalid response.\n" << std::endl;
+    }
+
+    
 }
 
 int main() {
@@ -91,17 +163,26 @@ int main() {
             //if the user simply presses enter, it will re-start the loop
         } else if (input == "?" || input == "help") {
             userManuel();
-        } else if (splitInput[0] == "create") {
+        } else if (input == "directory") {
+            showFileDirectory();
+        } else if (splitInput[1] != "" && splitInput[0] == "create") {
             createFile(splitInput[1]);
-        } else if (splitInput[0] == "read") {
+        } else if (splitInput[1] != "" && splitInput[0] == "read") {
             readFile(splitInput[1]);
-        } else if (splitInput[0] == "write") {
-        } else if (splitInput[0] == "delete") {
+        } else if (splitInput[1] != "" && splitInput[0] == "write") {
+            writeFile(splitInput[1]);
+        } else if (splitInput[1] != "" && splitInput[0] == "delete") {
+            deleteFile(splitInput[1]);
         } else if (input == "quit") {
             running = false;
         } else {
-            std::cerr << "ERROR: invalid command.\n";
+            std::cerr << "ERROR: invalid command.\n" << std::endl;
         }
+
+        //reset the input
+        input = "";
+        splitInput[0] = "";
+        splitInput[1] = "";
     }
     
     return 0;
